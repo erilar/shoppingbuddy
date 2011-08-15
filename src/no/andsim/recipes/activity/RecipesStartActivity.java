4b686894047f2ec164401ba1f567082b22b68031
simon.andresen <Simon.Royrvik.Andresen@iteraconsulting.no>
2011-08-15 10:39:59 +0200

    initial commit

---
+++ src/no/andsim/recipes/activity/RecipesStartActivity.java
@@ -0,0 +1,21 @@
+package no.andsim.recipes.activity;
+
+import no.andsim.recipes.util.SplashThread;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+public class RecipesStartActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.splash);
+		Intent nextActivity = new Intent("");
+		SplashThread logoSplash = new SplashThread(this, nextActivity, 5000);
+		logoSplash.start();
+	}
+
+}
---
+++ src/no/andsim/recipes/util/SplashThread.java
@@ -0,0 +1,35 @@
+package no.andsim.recipes.util;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class SplashThread extends Thread {
+	
+	private Activity creator;
+	private Intent nextActivity;
+	private int duration;
+	
+	public SplashThread(Activity creator, Intent nextActivity, int duration){
+		this.creator = creator;
+		this.nextActivity = nextActivity;
+		this.duration = duration;
+	}
+	
+	@Override
+	public void run(){
+		super.run();
+		try {
+			int logoTimer = 0;
+			while (logoTimer < duration) {
+				sleep(100);
+				logoTimer = logoTimer + 100;
+			}
+			 creator.startActivity(nextActivity);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			creator.finish();
+		}
+	}
+
+}
