cc06376a28a4a3de165270bc722909050488e854
erilar <simon.andresen@gmail.com>
2011-09-14 12:19:15 +0200

    Extracted VareServiceClient from activity using it

---
+++ src/no/andsim/recipes/activity/VareServiceClientActivity.java
@@ -0,0 +1,27 @@
+package no.andsim.recipes.activity;
+
+import no.andsim.recipes.model.Vare;
+import no.andsim.recipes.ws.VareServiceClient;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class VareServiceClientActivity extends Activity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.service);
+		TextView lblResult = (TextView) findViewById(R.id.wsresult);
+		VareServiceClient vareService = new VareServiceClient();
+		super.onCreate(savedInstanceState);
+
+		Vare vare = new Vare(2123123L, "Melk");
+		
+		String returnMessage = vareService.sendVareToWS(vare);
+		lblResult.setText(returnMessage);
+		
+
+	}
+
+}
--- src/no/andsim/recipes/ws/VareServiceClient.java
+++ src/no/andsim/recipes/ws/VareServiceClient.java
@@ -1,8 +1,7 @@
-package no.andsim.recipes.activity;
+package no.andsim.recipes.ws;
 
 import java.io.IOException;
 
-import no.andsim.recipes.activity.R;
 import no.andsim.recipes.model.Vare;
 
 import org.ksoap2.SoapEnvelope;
@@ -13,26 +12,17 @@ import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 import org.xmlpull.v1.XmlPullParserException;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class VareServiceClient extends Activity {
+public class VareServiceClient {
+	
 	private static final String NAMESPACE = "http://service.andsim.no/";
 	private static String URL = //"http://172.16.9.190:9191/VareService?wsdl";
 			"http://vareservice.herokuapp.com/VareService?wsdl";
 	private static final String METHOD_NAME = "sendVare";
 	private static final String SOAP_ACTION = "http://service.andsim.no/sendVare";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
 
-		super.onCreate(savedInstanceState);
-
-		Vare vare = new Vare(123123L, "Tran");
+	public String sendVareToWS(Vare vare) {
 		
-		setContentView(R.layout.service);
-		TextView lblResult = (TextView) findViewById(R.id.wsresult);
 		SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
 		PropertyInfo propInfo = new PropertyInfo();
 		propInfo.name = "arg0";
@@ -48,14 +38,13 @@ public class VareServiceClient extends Activity {
 			androidHttpTransport.call(SOAP_ACTION, envelope);
 			SoapPrimitive  resultsRequestSOAP = (SoapPrimitive) envelope.getResponse();
 			String returnMessage = resultsRequestSOAP.toString();
-			lblResult.setText(returnMessage);
+			return returnMessage;
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (XmlPullParserException e) {
 			e.printStackTrace();
 		}
-		
-		
-
+		return null;
 	}
+
 }
